f806c04355491672220c8cbd360f2735e030d1fa
TayaHatum <21taya21@gmail.com>
2022-08-17 13:09:21 +0300

    added start

---
+++ src/test/java/tests/DragTests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import config.AppiumConfiguration;
+import org.testng.annotations.Test;
+
+public class DragTests extends AppiumConfiguration {
+
+    @Test
+    public void test(){
+
+    }
+}
